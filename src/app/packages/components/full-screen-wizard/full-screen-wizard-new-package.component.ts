c934922d85264f6764d23af070a9161f29407495
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2024-08-21 23:33:22 -0600

    feat: wizard 3.0

--- src/app/packages/components/full-screen-wizard/full-screen-wizard-new-package.component.ts
+++ src/app/packages/components/full-screen-wizard/full-screen-wizard-new-package.component.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 export class FullScreenWizardNewPackageComponent implements OnInit {
   wizardForm: FormGroup = new FormGroup({});
 
+  showTransportSection = true;
+
   currentStep = 1;
 
   steps = [
@@ -19,6 +21,7 @@ export class FullScreenWizardNewPackageComponent implements OnInit {
         {
           name: 'name',
           label: '¿Cual es el nombre del paquete?',
+          hint: 'Ejemplo: Paquete para conocer Guadalajara',
           type: 'text',
           validators: [Validators.required],
           error: 'Name is required.',
@@ -26,6 +29,7 @@ export class FullScreenWizardNewPackageComponent implements OnInit {
         {
           name: 'price',
           label: '¿Cual es el precio del paquete?',
+          hint: 'Ejemplo: 1000',
           type: 'number',
           specialType: 'currency',
           validators: [Validators.required],
@@ -34,10 +38,11 @@ export class FullScreenWizardNewPackageComponent implements OnInit {
         {
           name: 'transport',
           label: '¿El paquete incluye transporte?',
+          hint: '',
           type: 'select',
           options: [
-            { label: 'Si', value: true },
-            { label: 'No', value: false },
+            { label: 'Si', value: '1' },
+            { label: 'No', value: '0' },
           ],
           validators: [Validators.required],
           error: 'Gender is required.',
@@ -45,10 +50,11 @@ export class FullScreenWizardNewPackageComponent implements OnInit {
         {
           name: 'hosting',
           label: '¿El paquete incluye alojamiento?',
+          hint: '',
           type: 'select',
           options: [
-            { label: 'Si', value: true },
-            { label: 'No', value: false },
+            { label: 'Si', value: '1' },
+            { label: 'No', value: '0' },
           ],
           validators: [Validators.required],
           error: 'Gender is required.',
@@ -56,35 +62,89 @@ export class FullScreenWizardNewPackageComponent implements OnInit {
         {
           name: 'limit',
           label: '¿Cual es el limite de personas?',
-          type: 'number',
+          hint: 'Ejemplo: 10',
+          type: 'text',
           validators: [Validators.required],
           error: 'Name is required.',
         },
       ],
     },
     {
-      title: 'Step 2 (Optional)',
+      title: 'Transporte',
       fields: [
         {
-          name: 'email',
-          label: 'Email',
-          type: 'text',
-          validators: [Validators.email],
-          error: 'Enter a valid email.',
+          name: 'transportType',
+          label: '¿Que tipo de transporte incluye el paquete?',
+          hint: '',
+          type: 'select',
+          options: [
+            { label: 'Vuelos', value: '1' },
+            { label: 'Trenes', value: '2' },
+            { label: 'Autobuses', value: '3' },
+            { label: 'Automóviles de alquiler', value: '4' },
+            { label: 'Transporte marítimo', value: '5' },
+            { label: 'Servicios de traslado', value: '6' },
+          ],
+          validators: [Validators.required],
+          error: 'Gender is required.',
+        },
+        {
+          name: 'transportDescription',
+          label: '¿Que descripción le pondrías al transporte?',
+          hint: 'Ejemplo: Paquete para conocer Guadalajara donde se incluye el transporte en avión y cuando llegues al destino se incluye el transporte en autobús.',
+          type: 'text-area',
+          validators: [Validators.required],
+          error: 'Name is required.',
+        },
+      ],
+    },
+    {
+      title: 'Hospedaje',
+      fields: [
+        {
+          name: 'hostingType',
+          label: '¿Que tipo de hospedaje incluye el paquete?',
+          hint: '',
+          type: 'select',
+          options: [
+            { label: 'Hoteles', value: '1' },
+            { label: 'Apartamento', value: '2' },
+            { label: 'Alojamiento en casas de huéspedes', value: '3' },
+            { label: 'Hostales', value: '4' },
+            { label: 'Resorts', value: '5' },
+            { label: 'Cabañas', value: '6' },
+            { label: 'Camping', value: '7' },
+            { label: 'Villas', value: '8' },
+          ],
+          validators: [Validators.required],
+          error: 'Gender is required.',
+        },
+        {
+          name: 'hotelDescription',
+          label: '¿Que descripción le pondrías al hospedaje?',
+          hint: 'Ejemplo: Paquete para conocer Guadalajara donde se incluye el hospedaje en hoteles de 5 estrellas.',
+          type: 'text-area',
+          validators: [Validators.required],
+          error: 'Name is required.',
+        },
+      ],
+    },
+    {
+      title: 'Descripción del paquete',
+      fields: [
+        {
+          name: 'generalDescription',
+          label: '¿Que descripción total le pondrías al paquete?',
+          hint: 'Ejemplo: Paquete para conocer Guadalajara donde se incluye el transporte en avión y cuando llegues al destino se incluye el transporte en autobús. Además se incluye el hospedaje en hoteles de 5 estrellas.',
+          type: 'text-area',
+          validators: [Validators.required],
+          error: 'Name is required.',
         },
       ],
     },
     {
       title: 'Step 3',
-      fields: [
-        {
-          name: 'password',
-          label: 'Password',
-          type: 'text',
-          validators: [Validators.required],
-          error: 'Password is required.',
-        },
-      ],
+      fields: [],
     },
   ];
 
@@ -97,8 +157,19 @@ export class FullScreenWizardNewPackageComponent implements OnInit {
       transport: ['', Validators.required],
       hosting: ['', Validators.required],
       limit: ['', Validators.required],
-      email: ['', Validators.email],
-      password: ['', Validators.required],
+      transportType: ['', Validators.required],
+      transportDescription: ['', Validators.required],
+      hostingType: ['', Validators.required],
+      hotelDescription: ['', Validators.required],
+      generalDescription: ['', Validators.required],
+    });
+
+    this.wizardForm.get('transport')?.valueChanges.subscribe((value) => {
+      this.filterTransportSteps(value);
+    });
+
+    this.wizardForm.get('hosting')?.valueChanges.subscribe((value) => {
+      this.filterHostingSteps(value);
     });
   }
 
@@ -120,12 +191,96 @@ export class FullScreenWizardNewPackageComponent implements OnInit {
   }
 
   onSubmit(): void {
-    if (this.wizardForm.valid) {
-      console.log('Form Submitted', this.wizardForm.value);
-    }
+    //if (this.wizardForm.valid) {
+    console.log('Form Submitted', this.wizardForm.value);
+    //}
   }
 
   goBackToNewPackage() {
     this.router.navigate(['paquetes']);
+  }
+
+  filterTransportSteps(transportValue: string): void {
+    if (transportValue === '0') {
+      this.steps = this.steps.filter((step) => step.title !== 'Transporte');
+    } else if (transportValue === '1') {
+      const insertIndex = this.steps.length - 2;
+      if (!this.steps.some((step) => step.title === 'Transporte')) {
+        this.steps.splice(insertIndex, 0, {
+          title: 'Transporte',
+          fields: [
+            {
+              name: 'transportType',
+              label: '¿Que tipo de transporte incluye el paquete?',
+              hint: '',
+              type: 'select',
+              options: [
+                { label: 'Vuelos', value: '1' },
+                { label: 'Trenes', value: '2' },
+                { label: 'Autobuses', value: '3' },
+                { label: 'Automóviles de alquiler', value: '4' },
+                { label: 'Transporte marítimo', value: '5' },
+                { label: 'Servicios de traslado', value: '6' },
+              ],
+              validators: [Validators.required],
+              error: 'Gender is required.',
+            },
+            {
+              name: 'transportDescription',
+              label: '¿Que descripción le pondrías al transporte?',
+              hint: 'Ejemplo: Paquete para conocer Guadalajara donde se incluye el transporte en avión y cuando llegues al destino se incluye el transporte en autobús.',
+              type: 'text-area',
+              validators: [Validators.required],
+              error: 'Name is required.',
+            },
+          ],
+        });
+      }
+    } else {
+      this.steps = [...this.steps];
+    }
+  }
+
+  filterHostingSteps(hostingValue: string): void {
+    if (hostingValue === '0') {
+      this.steps = this.steps.filter((step) => step.title !== 'Hospedaje');
+    } else if (hostingValue === '1') {
+      const insertIndex = this.steps.length - 2;
+      if (!this.steps.some((step) => step.title === 'Hospedaje')) {
+        this.steps.splice(insertIndex, 0, {
+          title: 'Hospedaje',
+          fields: [
+            {
+              name: 'hostingType',
+              label: '¿Que tipo de hospedaje incluye el paquete?',
+              hint: '',
+              type: 'select',
+              options: [
+                { label: 'Hoteles', value: '1' },
+                { label: 'Apartamento', value: '2' },
+                { label: 'Alojamiento en casas de huéspedes', value: '3' },
+                { label: 'Hostales', value: '4' },
+                { label: 'Resorts', value: '5' },
+                { label: 'Cabañas', value: '6' },
+                { label: 'Camping', value: '7' },
+                { label: 'Villas', value: '8' },
+              ],
+              validators: [Validators.required],
+              error: 'Gender is required.',
+            },
+            {
+              name: 'hotelDescription',
+              label: '¿Que descripción le pondrías al hospedaje?',
+              hint: 'Ejemplo: Paquete para conocer Guadalajara donde se incluye el hospedaje en hoteles de 5 estrellas.',
+              type: 'text-area',
+              validators: [Validators.required],
+              error: 'Name is required.',
+            },
+          ],
+        });
+      }
+    } else {
+      this.steps = [...this.steps];
+    }
   }
 }
